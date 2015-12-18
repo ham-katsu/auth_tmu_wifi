b8da0eb48ce52c82dfc52db231a5d9ff71830006
ham_katsu <ham-katsu@users.noreply.github.com>
2015-12-18 21:02:23 +0900

    modify main.py
    ループ内のウェイトタイムの変更
    
    デバッグ時に１０秒にしたままでした。。。

--- main.py
+++ main.py
@@ -2,15 +2,18 @@
 import urllib.parse
 import urllib.request
 import time
+
 url = 'http://1.1.1.1:10080/cgi-bin/adeflogin.cgi'
 auth_data = {'name' : 'yourID(u~~~~~~~)', 'pass' : 'your password' }
+
 d = urllib.parse.urlencode(auth_data)
-d = d.encode('Shift-JIS')
+d = d.encode('utf-8')
 header = {"Request URI":"/cgi-bin/adeflogin.cgi"}
 req = urllib.request.Request(url,d,header,method = "POST")
 print('ready')
+
 while(True):
     res = urllib.request.urlopen(req)
     print(res.getcode())
-    time.sleep(10)
+    time.sleep(3600)
 
